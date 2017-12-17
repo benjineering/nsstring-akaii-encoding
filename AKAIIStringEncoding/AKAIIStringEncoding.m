f632824915fb1fecb15c5ea405aabf7db0826096
Ben Williams <8enwilliams@gmail.com>
2017-12-17 12:01:08 +1100

    Initial Commit

---
+++ AKAIIStringEncoding/AKAIIStringEncoding.m
@@ -0,0 +1,13 @@
+//
+//  AKAIIStringEncoding.m
+//  AKAIIStringEncoding
+//
+//  Created by Ben Williams on 17/12/17.
+//  Copyright © 2017 Benjineering. All rights reserved.
+//
+
+#import "AKAIIStringEncoding.h"
+
+@implementation AKAIIStringEncoding
+
+@end
